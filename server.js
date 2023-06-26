6791e98ce17420a7278b0987ca72b4a5076281f3
Tony Izuogu <anthoizu@yahoo.com>
2023-06-25 14:47:52 -1100

    Created config file in dotenv for database connection string

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
-const mongodb = require('mongodb'); //Remove
+// const mongodb = require('mongodb'); //Remove
 
-mongoose.connect('mongodb://0.0.0.0:27017/subscribers', { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
