526fcff10146b5e27acc226b55a00b696637ce71
Tony Izuogu <anthoizu@yahoo.com>
2023-07-06 22:12:50 -1100

    Deleted comments

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose');
-// const mongodb = require('mongodb'); //Remove
+
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection
